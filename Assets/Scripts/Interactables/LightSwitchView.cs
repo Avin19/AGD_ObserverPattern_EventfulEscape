392f33bffea7dd9a06becb839339b8d1a44f25c4
Avin19 <avinashy1993@gmail.com>
2024-04-04 08:08:17 +0530

    event Action

--- Assets/Scripts/Interactables/LightSwitchView.cs
+++ Assets/Scripts/Interactables/LightSwitchView.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using static LightSwitchView;
@@ -6,16 +7,15 @@ public class LightSwitchView : MonoBehaviour, IInteractable
 {
     [SerializeField] private List<Light> lightsources = new List<Light>();
     private SwitchState currentState;
-    public delegate void LightSwitchDelegate();
-    public static LightSwitchDelegate lightToggled;
+    public static event Action lightToggleAction;
 
-    private void OnEnable() => lightToggled += onLightSwitch;
+    private void OnEnable() => lightToggleAction += onLightSwitch;
 
-    private void OnDisable() => lightToggled -= onLightSwitch;
+    private void OnDisable() => lightToggleAction -= onLightSwitch;
 
     private void Start() => currentState = SwitchState.Off;
 
-    public void Interact() => lightToggled?.Invoke();
+    public void Interact() => lightToggleAction?.Invoke();
 
     private void toggleLights()
     {
