8cf9730b2dc275f039e7502db44e7a16633e1085
Avinash Yadav <avinashy1993@gmail.com>
2025-05-31 10:17:14 +0530

    House Keeping #1

--- Assets/Scripts/UI/MainMenuUIView.cs
+++ Assets/Scripts/UI/MainMenuUIView.cs
@@ -1,3 +1,5 @@
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -5,7 +7,61 @@ using UnityEngine.UI;
 public class MainMenuUIView : MonoBehaviour
 {
     [SerializeField] private Button playButton;
+    [SerializeField] private Button settingBtn;
+    [SerializeField] private Button backbtn;
+    [SerializeField] private RectTransform settingPanel;
+    [SerializeField] private Slider slider;
+    [SerializeField] private Slider sliderSound;
+    [SerializeField] private TextMeshProUGUI textSens;
+    [SerializeField] private TextMeshProUGUI textSound;
+    [SerializeField] private PlayerScriptableObject playerScriptableObject;
+    private void Awake()
+    {
+        settingPanel.gameObject.SetActive(false);
+    }
+    private void OnEnable()
+    {
+        playButton.onClick.AddListener(OnPlayButtonClicked);
+        settingBtn.onClick.AddListener(OnSettingButtonClicked);
+        backbtn.onClick.AddListener(OnBAckButton);
 
-    private void OnEnable() => playButton.onClick.AddListener(OnPlayButtonClicked);
-    private void OnPlayButtonClicked() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void OnSlidervalueChangedSound(float arg0)
+    {
+        int value = (int)arg0;
+        textSound.text = value.ToString();
+    }
+
+    private void OnSliderValueChanged(float arg0)
+    {
+        int value = (int)arg0;
+        textSens.text = value.ToString();
+        playerScriptableObject.sensitivity = value;
+    }
+
+    private void OnBAckButton()
+    {
+        settingPanel.gameObject.SetActive(false);
+    }
+
+    private void OnSettingButtonClicked()
+    {
+        settingPanel.gameObject.SetActive(true);
+        sliderSound.onValueChanged.AddListener(OnSlidervalueChangedSound);
+        slider.onValueChanged.AddListener(OnSliderValueChanged);
+        textSens.text = slider.value.ToString();
+        textSound.text = sliderSound.value.ToString();
+    }
+
+    private void SaveMouseSetting()
+    {
+
+        textSens.text = slider.value.ToString();
+        playerScriptableObject.sensitivity = slider.value;
+    }
+    private void OnPlayButtonClicked()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 }
